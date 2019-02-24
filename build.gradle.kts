1466d8ca47a64e4b570f0b22e82210c00a89c2a8
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2019-02-24 23:51:10 +0300

    Add Gradle build scripts
    Former-commit-id: 4b32c2ef9eeb6afcdf69fc5cd8212ae933c299ac

---
+++ build.gradle.kts
@@ -0,0 +1,268 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import com.github.spotbugs.SpotBugsPlugin
+import com.github.spotbugs.SpotBugsTask
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import versions.BuildToolVersions
+
+plugins {
+    java
+    jacoco
+    checkstyle
+    id("org.nosphere.apache.rat") version "0.4.0"
+    id("com.github.ethankhall.semantic-versioning") version "1.1.0"
+    id("com.github.spotbugs") version "1.6.10"
+    id("org.sonarqube") version "2.7.1"
+    witness
+    signing
+    publishing
+    `stage-vote-release`
+}
+
+with(version as io.ehdev.version.Version) {
+    major = 5
+    minor = 2
+    patch = 0
+    releaseBuild = true
+}
+
+apply(from = "$rootDir/gradle/dependencyVerification.gradle.kts")
+apply(from = "$rootDir/gradle/release.gradle.kts")
+
+// Do not enable spotbugs by default. Execute it only when -PenableSpotBugs (or -DenableSpotbugs) is present
+fun Project.enableSpotBugs() = hasProperty("enableSpotBugs") || hasProperty("enableSpotbugs")
+
+fun reportsForHumans() = !(System.getenv()["CI"]?.toBoolean() ?: false)
+
+val lastEditYear by extra {
+    file("$rootDir/NOTICE")
+        .readLines()
+        .first { it.contains("Copyright") }
+        .let {
+            """Copyright \d{4}-(\d{4})""".toRegex()
+                .find(it)?.groupValues?.get(1)
+                ?: throw IllegalStateException("Unable to identify copyright year from $rootDir/NOTICE")
+        }
+}
+
+val rat by tasks.getting(org.nosphere.apache.rat.RatTask::class) {
+    excludes.set(rootDir.resolve("rat-excludes.txt").readLines())
+}
+
+releaseParams {
+    previewSiteContents.add(copySpec {
+        into("rat")
+        from(rat)
+    })
+}
+
+val jacocoReport by tasks.registering(JacocoReport::class) {
+    group = "Coverage reports"
+    description = "Generates an aggregate report from all subprojects"
+}
+
+allprojects {
+    if (project.path != ":src") {
+        tasks.register<DependencyInsightReportTask>("allDepInsight") {
+            group = LifecycleBasePlugin.BUILD_GROUP
+            description =
+                "Shows insights where the dependency is used. For instance: allDepInsight --configuration compile --dependency org.jsoup:jsoup"
+        }
+    }
+}
+
+sonarqube {
+    properties {
+        // See https://docs.sonarqube.org/display/SCAN/Analyzing+with+SonarQube+Scanner+for+Gradle#AnalyzingwithSonarQubeScannerforGradle-Configureanalysisproperties
+        property("sonar.sourceEncoding", "UTF-8")
+        val projectName = "JMeter"
+        property("sonar.projectName", projectName)
+        property("sonar.projectKey", "org.apache.jmeter:$projectName")
+        property("sonar.projectVersion", project.version.toString())
+        property("sonar.host.url", System.getenv()["SONAR_HOST_URL"] ?: "http://localhost:9000")
+        property("sonar.login", System.getenv()["SONAR_LOGIN"] ?: "")
+        property("sonar.password", System.getenv()["SONAR_PASSWORD"] ?: "")
+        property("sonar.links.homepage", "http://jmeter.apache.org")
+        property("sonar.links.ci", "https://builds.apache.org/job/JMeter-trunk/")
+        property("sonar.links.scm", "http://jmeter.apache.org/svnindex.html")
+        property("sonar.links.issue", "http://jmeter.apache.org/issues.html")
+    }
+}
+
+allprojects {
+    group = "org.apache.jmeter"
+    // JMeter ClassFinder parses "class.path" and tries to find jar names there,
+    // so we should produce jars without versions names for now
+    // version = rootProject.version
+    apply<CheckstylePlugin>()
+    apply<SigningPlugin>()
+    apply<SpotBugsPlugin>()
+
+    checkstyle {
+        toolVersion = BuildToolVersions.checkstyle
+    }
+
+    spotbugs {
+        toolVersion = BuildToolVersions.spotbugs
+    }
+
+    plugins.withType<JacocoPlugin> {
+        the<JacocoPluginExtension>().toolVersion = BuildToolVersions.jacoco
+
+        val testTasks = tasks.withType<Test>()
+        val javaExecTasks = tasks.withType<JavaExec>()
+        for (t in arrayOf(testTasks, javaExecTasks)) {
+            t.configureEach {
+                extensions.findByType<JacocoTaskExtension>()?.apply {
+                    // We don't want to collect coverage for third-party classes
+                    includes?.add("org.apache.jmeter.*")
+                    includes?.add("org.apache.jorphan.*")
+                    includes?.add("org.apache.commons.cli.*")
+                }
+            }
+        }
+
+        jacocoReport {
+            // Note: this creates a lazy collection
+            // Some of the projects might fail to create a file (e.g. no tests or no coverage),
+            // So we check for file existence. Otherwise JacocoMerge would fail
+            val execFiles =
+                files(testTasks, javaExecTasks).filter { it.exists() && it.name.endsWith(".exec") }
+            executionData(execFiles)
+        }
+
+        tasks.withType<JacocoReport>().configureEach {
+            reports {
+                html.isEnabled = reportsForHumans()
+                xml.isEnabled = !reportsForHumans()
+            }
+        }
+        // Add each project to combined report
+        configure<SourceSetContainer> {
+            val mainCode = main.get()
+            jacocoReport.configure {
+                additionalSourceDirs.from(mainCode.allJava.srcDirs)
+                sourceDirectories.from(mainCode.allSource.srcDirs)
+                // IllegalStateException: Can't add different class with same name: module-info
+                // https://github.com/jacoco/jacoco/issues/858
+                classDirectories.from(mainCode.output.asFileTree.matching {
+                    exclude("module-info.class")
+                })
+            }
+        }
+    }
+
+    tasks.withType<AbstractArchiveTask>().configureEach {
+        // Ensure builds are reproducible
+        isPreserveFileTimestamps = false
+        isReproducibleFileOrder = true
+        dirMode = "775".toInt(8)
+        fileMode = "664".toInt(8)
+    }
+
+    // Not all the modules use publishing plugin
+    plugins.withType<PublishingPlugin> {
+        // Sign all the published artifacts
+        signing {
+            sign(publishing.publications)
+        }
+    }
+
+    plugins.withType<JavaPlugin> {
+        // This block is executed right after `java` plugin is added to a project
+
+        repositories {
+            jcenter()
+            ivy {
+                url = uri("https://github.com/bulenkov/Darcula/raw/")
+                content {
+                    includeModule("bulenkov", "darcula")
+                }
+                patternLayout {
+                    artifact("[revision]/build/[module].[ext]")
+                }
+                metadataSources {
+                    artifact() // == don't try downloading .pom file from the repository
+                }
+            }
+        }
+
+        tasks {
+            withType<JavaCompile>().configureEach {
+                options.encoding = "UTF-8"
+            }
+            withType<ProcessResources>().configureEach {
+                // apply native2ascii conversion since Java 8 expects properties to have ascii symbols only
+                from(source) {
+                    include("**/*.properties")
+                    filteringCharset = "UTF-8"
+                    filter(org.apache.tools.ant.filters.EscapeUnicode::class)
+                }
+            }
+            withType<Jar>().configureEach {
+                into("META-INF") {
+                    from("$rootDir/LICENSE")
+                    from("$rootDir/NOTICE")
+                }
+                manifest {
+                    attributes["Specification-Title"] = "Apache JMeter"
+                    attributes["Specification-Vendor"] = "Apache Software Foundation"
+                    attributes["Implementation-Vendor"] = "Apache Software Foundation"
+                    attributes["Implementation-Vendor-Id"] = "org.apache"
+                    attributes["Implementation-Version"] = project.version
+                }
+            }
+            withType<Test>().configureEach {
+                testLogging {
+                    exceptionFormat = TestExceptionFormat.FULL
+                }
+                // Pass the property to tests
+                systemProperty("java.awt.headless", System.getProperty("java.awt.headless"))
+            }
+            withType<SpotBugsTask>().configureEach {
+                reports {
+                    html.isEnabled = reportsForHumans()
+                    xml.isEnabled = !reportsForHumans()
+                }
+                enabled = enableSpotBugs()
+            }
+            withType<Javadoc>().configureEach {
+                (options as StandardJavadocDocletOptions).apply {
+                    noTimestamp.value = true
+                    showFromProtected()
+                    docEncoding = "UTF-8"
+                    charSet = "UTF-8"
+                    encoding = "UTF-8"
+                    docTitle = "Apache JMeter ${project.name} API"
+                    windowTitle = "Apache JMeter ${project.name} API"
+                    header = "<b>Apache JMeter</b>"
+                    bottom =
+                        "Copyright © 1998-$lastEditYear Apache Software Foundation. All Rights Reserved."
+                    links("https://docs.oracle.com/javase/8/docs/api/")
+                }
+            }
+        }
+
+        configure<JavaPluginConvention> {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+        }
+    }
+}
+
+
---
+++ src/build.gradle.kts
@@ -0,0 +1,123 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import versions.Libs
+
+subprojects {
+    apply<JavaPlugin>()
+    apply<GroovyPlugin>()
+    apply<MavenPublishPlugin>()
+    apply<JacocoPlugin>()
+
+    dependencies {
+        val testImplementation by configurations
+        testImplementation(Libs.junit)
+        testImplementation(Libs.groovy_all) {
+            because("We want to enable Groovy-based tests")
+        }
+        testImplementation(Libs.spock_core)
+        testImplementation(Libs.cglib_nodep) {
+            because("""
+                org.spockframework.mock.CannotCreateMockException: Cannot create mock for
+                 class org.apache.jmeter.report.processor.AbstractSummaryConsumer${'$'}SummaryInfo.
+                 Mocking of non-interface types requires a code generation library.
+                 Please put an up-to-date version of byte-buddy or cglib-nodep on the class path.""".trimIndent())
+        }
+        testImplementation(Libs.objenesis) {
+            because("""
+                org.spockframework.mock.CannotCreateMockException: Cannot create mock for
+                 class org.apache.jmeter.report.core.Sample. To solve this problem,
+                 put Objenesis 1.2 or higher on the class path (recommended),
+                 or supply constructor arguments (e.g. 'constructorArgs: [42]') that allow to construct
+                 an object of the mocked type.""".trimIndent())
+        }
+    }
+
+    val testJar by tasks.registering(Jar::class) {
+        val sourceSets: SourceSetContainer by project
+        archiveClassifier.set("test")
+        from(sourceSets["test"].output)
+    }
+
+    val sourcesJar by tasks.registering(Jar::class) {
+        val sourceSets: SourceSetContainer by project
+        from(sourceSets["main"].allJava)
+        archiveClassifier.set("sources")
+    }
+
+    val javadocJar by tasks.registering(Jar::class) {
+        from(tasks.named(JavaPlugin.JAVADOC_TASK_NAME))
+        archiveClassifier.set("javadoc")
+    }
+
+    val testClasses by configurations.creating {
+        extendsFrom(configurations["testRuntime"])
+    }
+
+    // Parenthesis needed to use Project#getArtifacts
+    (artifacts) {
+        testClasses(testJar)
+    }
+
+    val achiveBaseName = when (name) {
+        "jorphan" -> name
+        "launcher" -> "ApacheJMeter"
+        else -> "ApacheJMeter_$name"
+    }
+    setProperty("archivesBaseName", achiveBaseName)
+
+    // See https://stackoverflow.com/a/53661897/1261287
+    // Subprojects can't use "publishing" since that accessor is not available at parent project
+    // evaluation time
+    configure<PublishingExtension> {
+        publications {
+            create<MavenPublication>(project.name) {
+                artifactId = achiveBaseName
+                version = rootProject.version.toString()
+                from(components["java"])
+
+                // Eager task creation is required due to
+                // https://github.com/gradle/gradle/issues/6246
+                artifact(sourcesJar.get())
+                artifact(javadocJar.get())
+
+                pom {
+                    inceptionYear.set("1998")
+                    url.set("http://jmeter.apache.org/")
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                            comments.set("A business-friendly OSS license")
+                        }
+                    }
+                    issueManagement {
+                        system.set("bugzilla")
+                        url.set("https://bz.apache.org/bugzilla/describecomponents.cgi?product=JMeter")
+                    }
+                    scm {
+                        connection.set("scm:git:https://gitbox.apache.org/repos/asf/jmeter.git")
+                        developerConnection.set("scm:git:https://gitbox.apache.org/repos/asf/jmeter.git")
+                        url.set("https://github.com/apache/jmeter")
+                        tag.set("HEAD")
+                    }
+                }
+            }
+        }
+    }
+}
