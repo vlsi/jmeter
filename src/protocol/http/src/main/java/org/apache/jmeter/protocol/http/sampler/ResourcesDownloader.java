53b48c04a6ff474746097a12af23a80d1493162d
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2019-10-04 18:07:24 +0300

    Uncover ResourcesDownloader#init

--- src/protocol/http/src/main/java/org/apache/jmeter/protocol/http/sampler/ResourcesDownloader.java
+++ src/protocol/http/src/main/java/org/apache/jmeter/protocol/http/sampler/ResourcesDownloader.java
@@ -90,6 +90,9 @@ public class ResourcesDownloader {
 
     private void init() {
         LOG.info("Creating ResourcesDownloader with keepalive_inseconds : {}", THREAD_KEEP_ALIVE_TIME);
+        if (true) {
+            throw new RuntimeException("ResourcesDownloader#init() is covered");
+        }
         concurrentExecutor = new ThreadPoolExecutor(
                 MIN_POOL_SIZE, MAX_POOL_SIZE, THREAD_KEEP_ALIVE_TIME, TimeUnit.SECONDS,
                 new SynchronousQueue<>(),
